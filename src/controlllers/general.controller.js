f12c70a385f0759d7fcd9611b801b458bb40666d
jawuermkr <guitarro92@hotmail.com>
2023-01-20 10:37:17 -0500

    Controlador delete

--- src/controlllers/general.controller.js
+++ src/controlllers/general.controller.js
@@ -18,3 +18,23 @@ exports.createGeneral = (req, res) => {
     )
   })
 }
+
+exports.delete = (req, res) => {
+  req.getConnection((err, conn) =>{
+    if(err) return res.send(err);
+    conn.query(
+      "DELETE FROM " + req.params.tabla + " WHERE " + req.params.id + " = ? ", [req.params.value],
+      (err, rows) =>{
+        console.log(
+          err ? "Err DELETE FROM " + req.params.tabla + " WHERE " + req.params.id + " = ? " + err
+          : req.params.tabla + "ID eliminado correctamente."
+        );
+        res.json(
+          err
+          ? {err: "Ocurrio un error al tratar de eliminar."}
+          : {msg: "ID eliminado correctamente."}
+        )
+      }
+    )
+  })
+}
--- src/routes/general.routes.js
+++ src/routes/general.routes.js
@@ -4,5 +4,6 @@ const routes = express.Router();
 const generalController = require("../controlllers/general.controller");
 
 routes.post("/create/:tabla", generalController.createGeneral);
+routes.delete("/delete/:tabla/:id/:value", generalController.delete);
 
 module.exports = routes;
